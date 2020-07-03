6c9e143a6379a73dfdd0d3e045e2c9d2f804667b
Nguyễn Quang Huy <huy_nq@ehicas.com.vn>
2020-07-03 14:23:53 +0700

    end of 4 @ContentChild and ngAfterContentInit == begin of 5 @ContentChildren and QueryLists

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ContentChild, AfterContentInit } from '@angular/core';
 
 import { User } from './auth-form.interface';
+import { AuthRememberComponent } from './auth-remember.component';
 
 @Component({
   selector: 'auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent implements OnInit, AfterContentInit {
+  showMessage: boolean;
+  
+  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
+
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    if (this.remember) {
+      this.remember.checked.subscribe((checked: boolean) => {
+        this.showMessage = checked;
+      });
+    }
   }
 
   onSubmit(value: User) {
