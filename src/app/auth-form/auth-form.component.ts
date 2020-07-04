1dbf9c3b364a8ffb9e633faeb7ac748528145ec8
Nguyễn Quang Huy <huy_nq@ehicas.com.vn>
2020-07-04 09:02:44 +0700

    end of 7 @ViewChildren and QueryList == begin of 8 @ViewChild and template #ref

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, ContentChild, AfterContentInit, ContentChildren, QueryList, ViewChild, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ContentChild, AfterContentInit, ContentChildren, QueryList, ViewChild, ViewChildren, AfterViewInit, Input, ElementRef, ViewContainerRef, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 
 import { User } from './auth-form.interface';
 import { AuthRememberComponent } from './auth-remember.component';
@@ -14,39 +14,66 @@ export class AuthFormComponent implements OnInit, AfterContentInit, AfterViewIni
   
   // @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
   @ContentChildren(AuthRememberComponent) remember: QueryList<AuthRememberComponent> ;
-
-  @ViewChild(AuthMessageComponent, {static: true}) message : AuthMessageComponent;
-
+  
+  // @ViewChild(AuthMessageComponent, {static: true}) message : AuthMessageComponent;
+  
+  @ViewChildren(AuthMessageComponent) message : QueryList<AuthMessageComponent>;
+  // @ViewChildren(AuthMessageComponent,{read: ElementRef}) message : QueryList<AuthMessageComponent>;
+  // @ViewChildren(AuthMessageComponent,{read: AuthMessageComponent}) message : QueryList<AuthMessageComponent>;
+  // @ViewChildren(AuthMessageComponent,{read: ViewContainerRef}) message : QueryList<AuthMessageComponent>;
+  
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-
-  constructor() { }
-
+  
+  constructor(private cd: ChangeDetectorRef) { }
+  
   ngOnInit(): void {
   }
-
+  
   ngAfterViewInit() {
     // console.log(this.message);
     // this.message.days = 30;
-  }
-
-  ngAfterContentInit() {
+    
+    // if (this.message) {
+    //   this.message.days = 30;
+    // }
+    
+    // if (this.message) {
+    //   setTimeout(() => {
+    //     this.message.forEach((message) => {
+    //       message.days = 30;
+    //     });
+    //   });
+    // }
+    
     if (this.message) {
-      this.message.days = 30;
-    }
+      this.message.forEach((message) => {
+        message.days = 30;
+      });
 
+      this.cd.detectChanges();
+    }
+    
+  }
+  
+  
+  ngAfterContentInit() {
+    // if (this.message) {
+    //   this.message.days = 30;
+    // }
+    
     if (this.remember) {
       // this.remember.checked.subscribe((checked: boolean) => {
       //   this.showMessage = checked;
       // });
-
+      
       this.remember.forEach((item) => {
         item.checked.subscribe((checked: boolean) => this.showMessage = checked);
       });
     }
   }
-
+  
   onSubmit(value: User) {
     this.submitted.emit(value);
   }
-
+  
 }
